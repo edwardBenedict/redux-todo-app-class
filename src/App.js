4cc88d361fa6dbffadeedf3c355051d0ccfda480
ed ben <edwardbenedict92@gmail.com>
2021-05-26 23:50:52 +0300

    feat: finish project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">test</header>
+      <h1>Clarus Todos</h1>
+      <TodoInput />
+      <TodoList />
     </div>
   );
 }
